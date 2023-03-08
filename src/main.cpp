65bbaa25bc94f5f18746ea6f647be4ca823aa867
CharLee Desktop <charl@DESKTOP-B9PCFHN>
2023-03-08 10:13:17 -0800

    added some sample starting code

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,18 @@
 #include <stdio.h>
 #include "arm_math.h"
 
+// constants
+#define SAMPLE_RATE       16000
+#define FFT_SIZE          256
+#define INPUT_BUFFER_SIZE 64
+#define INPUT_SHIFT       2
+
+#define FFT_BINS_SKIP     5
+#define FFT_MAG_MAX       2000.0
+
+q15_t capture_buffer_q15[INPUT_BUFFER_SIZE];
+volatile int new_samples_captured = 0;
+
 void setup() {
   // put your setup code here, to run once:
 }
